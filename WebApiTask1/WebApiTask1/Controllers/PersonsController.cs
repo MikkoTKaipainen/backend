11c83564f464ad6525c55f108520c95ed3024a52
Mikko Kaipainen <mikko.kaipainen@student.saimia.fi>
2019-01-10 08:21:29 +0200

    Updates
    updated controllers

---
+++ WebApiTask1/WebApiTask1/Controllers/PersonsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTask1.Models;
+
+namespace WebApiTask1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAllPersons()
+        {
+            var persons = new List<Person>
+            {
+                new Person("Aku Ankka", 30),
+                new Person("Roope Setä", 60)
+            };
+
+            return new JsonResult(persons);
+        }
+    }
+}
--- WebApiTask1/WebApiTask1/Startup.cs
+++ WebApiTask1/WebApiTask1/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiTask1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiTask1
 {
@@ -25,6 +27,8 @@ namespace WebApiTask1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PersondbContext>(opt =>
+            opt.UseSqlServer(Configuration.GetConnectionString("LocalPersonDbContext")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
